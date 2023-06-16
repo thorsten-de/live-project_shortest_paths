c545dcf48a0094cd1fc0f098eedc6eec62862064
Thorsten Deienrt <thorsten.deinert@udo.edu>
2023-06-16 21:49:58 +0200

    Copy project and rename it

---
+++ generate_flow_networks/ShortestPaths/Node.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FlowNetworks
+{
+  internal class Node
+  {
+    public int Index { get; internal set; }
+    public Network Network { get; private set; }
+    public Point Center { get; private set; }
+    public string Text { get; set; }
+    public IList<Link> Links { get; private set; }
+
+    internal double TotalCost { get; set; }
+    internal Link? ShortestPathLink { get; set; }
+
+    public Node(Network network, Point center, string text)
+    {
+      Links = new List<Link>();
+      Network = network;
+      Center = center;
+      Text = text;
+      Index = -1;
+      Network.AddNode(this);
+    }
+
+    public override string ToString()
+    {
+      return $"[{Text}]";
+    }
+
+    public void AddLink(Link link) => Links.Add(link);
+
+
+    public Brush Stroke { get; set; } = Brushes.Black;
+    public double StrokeThickness { get; set; } = 2.0;
+
+    public Brush Background { get; set; } = Brushes.White;
+    public Brush Foregrond { get; set; } = Brushes.SteelBlue;
+
+    public const double LARGE_RADIUS = 10;
+    public const double SMALL_RADIUS = 3;
+
+    public void Draw(Canvas canvas, bool drawLabels)
+    {
+      double radius = drawLabels ? LARGE_RADIUS : SMALL_RADIUS;
+      MyEllipse = canvas.DrawEllipse(Center.CenteredBounds(radius), Background, Stroke, StrokeThickness);
+      MyEllipse.Tag = this;
+      MyEllipse.MouseDown += Network.node_MouseDown;
+
+      if (drawLabels)
+      {
+        MyLabel = canvas.DrawString(Text, 2 * radius, 2 * radius, Center, 0, 12, Foregrond);
+        MyLabel.Tag = this;
+        MyLabel.MouseDown += Network.node_MouseDown;
+      }
+    }
+
+    private Ellipse MyEllipse { get; set; }
+    private Label MyLabel { get; set; }
+
+    private bool isStartNode = false;
+    public bool IsStartNode
+    {
+      get => isStartNode;
+      set
+      {
+        isStartNode = value;
+        SetNodeAppearance();
+      }
+    }
+
+    private bool isEndNode = false;
+    public bool IsEndNode
+    {
+      get => isEndNode;
+      set
+      {
+        isEndNode = value;
+        SetNodeAppearance();
+      }
+    }
+
+    public void SetNodeAppearance()
+    {
+      if (MyEllipse == null) return;
+
+      if (isStartNode)
+      {
+        MyEllipse.Fill = Brushes.Pink;
+        MyEllipse.Stroke = Brushes.Red;
+        MyEllipse.StrokeThickness = 2;
+      }
+      else
+      if (isEndNode)
+      {
+        MyEllipse.Fill = Brushes.LightGreen;
+        MyEllipse.Stroke = Brushes.Green;
+        MyEllipse.StrokeThickness = 2;
+      }
+      else
+      {
+        MyEllipse.Fill = Brushes.White;
+        MyEllipse.Stroke = Brushes.Black;
+        MyEllipse.StrokeThickness = 1;
+      }
+
+    }
+  }
+}
---
+++ generate_flow_networks/ShortestPaths/PathAlgorithms.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace FlowNetworks
+{
+  /// <summary>
+  /// Stategy Pattern for Path Algorithms on Networks
+  /// </summary>
+  interface PathAlgorithm
+  {
+    void FindPathTree(Network network);
+  }
+
+  public class PathAlgorithms
+  {
+    internal static readonly PathAlgorithm LabelSetting = new LabelSettingStrategy();
+    internal static readonly PathAlgorithm LabelSettingPrio = new LabelSettingPriorityQueueStrategy();
+    internal static readonly PathAlgorithm LabelCorrection = new LabelCorrectingStrategy();
+    internal static readonly PathAlgorithm[] All = new[] { LabelSetting, LabelSettingPrio, LabelCorrection };
+
+
+    private class LabelSettingStrategy : PathAlgorithm
+    {
+      public override string ToString() => "Label Setting";
+
+      public void FindPathTree(Network network)
+      {
+        int checks = 0, pops = 0;
+        var queue = new List<Node>();
+
+        queue.Add(network.StartNode);
+        var visited = new HashSet<Node>();
+
+        while (queue.Count > 0)
+        {
+          Node u = queue.MinBy(n => n.TotalCost);
+          checks += queue.Count;
+
+          queue.Remove(u);
+          visited.Add(u);
+          pops++;
+
+          foreach (var link in u.Links)
+          {
+            var new_cost = u.TotalCost + link.Cost;
+            var v = link.ToNode;
+            if (!visited.Contains(v) && new_cost < v.TotalCost)
+            {
+              v.TotalCost = new_cost;
+              v.ShortestPathLink = link;
+              queue.Add(v);
+            }
+          }
+        }
+
+        Debug.WriteLine("{0}: {1} checks, {2} pops", this, checks, pops);
+      }
+    }
+
+    private class LabelSettingPriorityQueueStrategy : PathAlgorithm
+    {
+      public override string ToString() => "Label Setting (PriorityQueue)";
+
+      public void FindPathTree(Network network)
+      {
+        int checks = 0, pops = 0;
+        var queue = new PriorityQueue<Node, double>();
+        queue.Enqueue(network.StartNode, 0);
+        
+        var visited = new HashSet<Node>();
+
+        while (queue.Count > 0)
+        {
+          Node u = queue.Dequeue();
+          visited.Add(u);
+          pops++;
+
+          foreach (var link in u.Links)
+          {
+            var new_cost = u.TotalCost + link.Cost;
+            var v = link.ToNode;
+            if (!visited.Contains(v) && new_cost < v.TotalCost)
+            {
+              v.TotalCost = new_cost;
+              v.ShortestPathLink = link;
+              queue.Enqueue(v, v.TotalCost);
+            }
+          }
+        }
+
+        Debug.WriteLine("{0}: {1} checks, {2} pops", this, checks, pops);
+      }
+    }
+
+    private class LabelCorrectingStrategy : PathAlgorithm
+    {
+      public override string ToString() => "Label Correcting";
+
+      public void FindPathTree(Network network)
+      {
+        int checks = 0, pops = 0;
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(network.StartNode);
+
+        while (queue.Any())
+        {
+          Node u = queue.Dequeue();
+          pops++;
+
+          foreach (var link in u.Links)
+          {
+            var new_cost = u.TotalCost + link.Cost;
+            var v = link.ToNode;
+            if (new_cost < v.TotalCost)
+            {
+              v.TotalCost = new_cost;
+              v.ShortestPathLink = link;
+              queue.Enqueue(v);
+            }
+          }
+        }
+
+        Debug.WriteLine("{0}: {1} checks, {2} pops", this, checks, pops);
+      }
+    }
+  }
+}
