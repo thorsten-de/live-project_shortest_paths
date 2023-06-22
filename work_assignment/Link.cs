372dc40434077e317e0d07ea58c88f20baf60b35
Thorsten Deienrt <thorsten.deinert@udo.edu>
2023-06-22 22:23:29 +0200

    Copy flow_networks to work_assignments

---
+++ work_assignment/Link.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace FlowNetworks;
+
+internal class Link
+{
+    private const double RADIUS = 10;
+
+    private readonly Network _network;
+    private readonly Node _fromNode;
+    private readonly Node _toNode;
+    private readonly double _capacity;
+    private double _flow;
+    
+    private Brush _textBrush = Brushes.Black;
+    private Brush _stroke = Brushes.Silver;
+    private double _strokeThickness = 2;
+    private Line _myLine;
+
+    public Link(Network network, Node fromNode, Node toNode, int capacity)
+    {
+        _network = network;
+        _fromNode = fromNode;
+        _toNode = toNode;
+        _capacity = capacity;
+        Flow = 0;
+
+        FromNode.AddLink(this);
+        ToNode.AddBacklink(this);
+        Network.AddLink(this);
+    }
+
+    public Network Network => _network;
+
+    public Node FromNode => _fromNode;
+
+    public Node ToNode => _toNode;
+
+    public double Capacity => _capacity;
+
+    public double Flow
+    {
+        get => _flow;
+        set
+        {
+            _flow = value; 
+            SetLinkAppearance();
+        }
+    }
+
+    public void UseRegularFrom(Node from)
+    {
+        VisitNode(ToNode, from);
+    }
+
+    public void UseBacklinkFrom(Node from)
+    {
+        VisitNode(FromNode, from);
+        IsBacklink = true;
+    }
+
+    private void VisitNode(Node target, Node from)
+    {
+        target.FromNode = from;
+        target.FromLink = this;
+        target.Visited = true;
+    }
+
+    public void AlterFlow(double delta) =>
+        Flow += IsBacklink ? -delta : +delta;
+
+    public double ResidualCapacity => 
+        Capacity - Flow;
+
+    public double Delta => IsBacklink ? Flow : ResidualCapacity;
+
+    public bool IsBacklink { get; set; }
+    
+    public Brush TextBrush
+    {
+        get => _textBrush;
+        set => _textBrush = value;
+    }
+
+    public Brush Stroke
+    {
+        get => _stroke;
+        set => _stroke = value;
+    }
+
+    public double StrokeThickness
+    {
+        get => _strokeThickness;
+        set => _strokeThickness = value;
+    }
+
+    private Line MyLine
+    {
+        get => _myLine;
+        set => _myLine = value;
+    }
+
+    public override string ToString() =>
+        $"{FromNode} {(IsBacklink ? "<--" : "-->")} {ToNode} ({Flow}/{Capacity})";
+
+    public void Draw(Canvas canvas)
+    {
+        canvas.DrawLine(FromNode.Center, ToNode.Center, Stroke, 2 * Capacity);
+        MyLine = canvas.DrawLine(FromNode.Center, ToNode.Center, Stroke, StrokeThickness);
+    }
+
+    public void DrawLabel(Canvas canvas)
+    {
+        var d = ToNode.Center - FromNode.Center;
+        var angle = Math.Atan2(d.Y, d.X) * 180 / Math.PI;
+
+        var c = FromNode.Center + d / 3;
+
+        canvas.DrawEllipse(c.CenteredBounds(RADIUS), Brushes.White, Brushes.White, 0);
+        MyLabel = canvas.DrawString(Display, 2 * RADIUS, 2 * RADIUS, c, angle, 12, TextBrush);
+    }
+
+    private string Display => $"{Flow}/{Capacity}";
+
+    private Label MyLabel;
+
+    private void SetLinkAppearance()
+    {
+        if (MyLine == null) return;
+
+        if (Flow > 0)
+        {
+            MyLine.Stroke = Brushes.Red;
+            MyLine.StrokeThickness = 2 * Flow;
+        }
+        else
+        {
+            MyLine.Stroke = Stroke;
+            MyLine.StrokeThickness = StrokeThickness;
+        }
+
+        if (MyLabel != null)
+        {
+            MyLabel.Content = Display;
+        }
+    }
+}
---
+++ work_assignment/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace FlowNetworks;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Network MyNetwork = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = true;
+    }
+
+    private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        try
+        {
+            var dialog =
+                new OpenFileDialog
+                {
+                    DefaultExt = ".net",
+                    Filter = "Network Files|*.net|All Files|*.*"
+                };
+            if (dialog.ShowDialog() == true) MyNetwork = Network.FromFile(dialog.FileName);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+            MyNetwork = new Network();
+        }
+
+        DrawNetwork();
+    }
+
+    private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void DrawNetwork()
+    {
+        mainCanvas.Children.Clear();
+        MyNetwork.Draw(mainCanvas);
+    }
+}
